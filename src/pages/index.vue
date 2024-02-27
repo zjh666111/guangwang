0e805f29a6042e678095aa0e9cc93d4fa2835130
yangyang <90810016+15982822539@users.noreply.github.com>
2024-02-26 20:49:54 -0800

    new

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,7 @@ defineOptions({
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        639293382@qq.com
+        Samrathjain@icloud.com
       </div>
     </div>
   </div>
