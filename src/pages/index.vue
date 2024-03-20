13e961a2f48609eace3e93c08ebfb9e59cf32c75
yangyang <790372987@qq.com>
2024-03-19 22:34:31 -0700

    new

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,7 @@ defineOptions({
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
       <div rd-2 bg-transparent p-2 p-x-5 text-5 style="color: #F5E400;">
-        639293382@qq.com
+        hemmesadloff862@gmail.com
       </div>
     </div>
   </div>
