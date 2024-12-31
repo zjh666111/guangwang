8d912534d2342720b425698baf9cfac5066630e7
zjh666111 <2601255328@qq.com>
2024-12-31 09:37:41 +0800

    官网邮箱修改

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,7 @@ defineOptions({
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        可以上架
+        yy4553211@icloud.com
       </div>
     </div>
   </div>
