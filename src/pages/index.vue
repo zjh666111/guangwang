929b67ad59b30c5bd948d18bb98169be55061fed
zjh666111 <2601255328@qq.com>
2025-01-06 16:11:01 +0800

    bg修改

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,7 +6,7 @@ defineOptions({
 
 <template>
   <div relative min-h-100vh>
-    <img h-full w-full src="../assets/bg.webp" alt="">
+    <img h-full w-full src="../assets/bg.png" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
