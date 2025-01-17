7cf44a470cfab0bce747cb6ea677e2f1d9d05e16
zjh666111 <2601255328@qq.com>
2025-01-17 14:45:02 +0800

    邮箱修改

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,7 @@ defineOptions({
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        639293382@qq.com
+        yy20009220@icloud.com
       </div>
     </div>
   </div>
