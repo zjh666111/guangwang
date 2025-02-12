99ab6146c1ca749dd5d0ea3755a8961217b62c8c
zjh666111 <2601255328@qq.com>
2025-02-12 17:08:18 +0800

    email_change

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,7 @@ defineOptions({
 
     <div class="email" absolute top-10 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        639293382@qq.com
+        lc9917733@icloud.com
       </div>
     </div>
   </div>
