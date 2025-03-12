948c5151f47b655d1268dbded14aa86f9216bfe9
zjh666111 <2601255328@qq.com>
2025-03-12 14:15:48 +0800

    test1修改

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,9 +8,9 @@ defineOptions({
   <div relative min-h-100vh>
     <img h-full w-full src="../assets/bg.webp" alt="">
 
-    <div class="email" absolute bottom-10 w-full flex justify-center>
+    <div class="email" absolute top-10 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        639293382@qq.com
+        可以上架
       </div>
     </div>
   </div>
