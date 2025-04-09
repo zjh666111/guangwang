121b85a4cbea972fc03d452595b1934b5e133ae7
zjh666111 <2601255328@qq.com>
2025-04-09 16:26:30 +0800

    官网修改

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,7 +8,7 @@ defineOptions({
   <div relative min-h-100vh>
     <img h-full w-full src="../assets/bg.png" alt="">
 
-    <div class="email" absolute bottom-10 w-full flex justify-center>
+    <div class="email" absolute top-10 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
         可以上架
       </div>
